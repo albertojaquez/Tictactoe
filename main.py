a3a938f18ac47f353209c5386a069ec9afc5c962
albertojaquez <36212419+albertojaquez@users.noreply.github.com>
2022-01-26 11:29:25 -0800

    Comment main

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import time
 import tkinter as tk
 from tkinter import ttk
 
-ventanaInicio=tk.Tk()
+#Frame
+ventanaInicio = tk.Tk()
 ventanaInicio.title("Tic Tac Toe")
 ventanaInicio.configure(bg='black')
 ventanaInicio.geometry("300x400+618+232")
@@ -42,6 +43,7 @@ def JugarCPU():
     BotonRegresar = ttk.Button(ventanaInicio, text="Regresar", style="MyButton.TButton", command=BotonRegresar)
     BotonRegresar.place(x=190, y=355, width=100, height=35)
 
+#Function to play
 def SalirJuego():
     ventanaInicio.destroy()
 #def ventanaJugar():
@@ -52,13 +54,17 @@ def SalirJuego():
 #LogoJuego=tkinter.Label(ventanaInicio,image=tkinter.PhotoImage(file="LogoNombre.jpg"))
 #LogoJuego.pack()
 
-BotonJugarCPU=ttk.Button(ventanaInicio,text="J1vs CPU",style="MyButton.TButton",command=JugarCPU)
+#Create button to play with CPU
+BotonJugarCPU = ttk.Button(ventanaInicio,text="J1vs CPU",style="MyButton.TButton",command=JugarCPU)
 BotonJugarCPU.place(x=25,y=150,width=250,height=35)
 
+#Create button to play with a friend
 BotonJugarJ2=ttk.Button(ventanaInicio,text="J1 vs J2",style="MyButton.TButton",command=JugarJ2)
 BotonJugarJ2.place(x=25,y=190,width=250,height=35)
 
+#Create button to quit
 BotonSalir=ttk.Button(ventanaInicio,text="Salir",style="MyButton.TButton",command=SalirJuego)
 BotonSalir.place(x=25,y=230,width=250,height=35)
 
+#Main
 ventanaInicio.mainloop()
